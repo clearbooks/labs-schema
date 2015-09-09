bcaa3d0a2e8198511a083d85a1fbb25c84b896cf
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-09-09 11:42:06 +0100

    Making VARCHAR(128) and making it Promary Key

--- sql/2015-09-09-11-20-subscriber-schema.sql
+++ sql/2015-09-09-11-20-subscriber-schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `subscribers` (
-	`user_id` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci'
+	`user_id` VARCHAR(128) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	PRIMARY KEY (`user_id`)
 )
 COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB;
