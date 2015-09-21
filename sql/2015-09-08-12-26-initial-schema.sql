ed1d7fb3fd409d28ae30d2958894450e9ce3cb9f
Vlodymyr Sergeyev <volodymyr@clearbooks.co.uk>
2015-09-21 15:38:35 +0100

    Adding Foreign keys to the schema and changing the structure. renaming the one of the files.

--- sql/2015-09-08-12-26-initial-schema.sql
+++ sql/2015-09-08-12-26-initial-schema.sql
@@ -1,28 +1,51 @@
-CREATE TABLE IF NOT EXISTS `group_policy` (
-  `group_id` varchar(128) NOT NULL,
-  `toggle_id` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`group_id`,`toggle_id`),
-  KEY `active` (`active`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `release` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`info` VARCHAR(255) NOT NULL,
+	`visibility` TINYINT(1) NOT NULL DEFAULT 1,
+	`release_date` DATE NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `toggle` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `type` enum('simple','group') NOT NULL DEFAULT 'simple',
-  `visible` tinyint(1) NOT NULL DEFAULT '0',
-  `release_id` int(11) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `type` (`type`),
-  KEY `visible` (`visible`),
-  KEY `release_id` (`release_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(128) NOT NULL,
+	`type` ENUM('simple','group') NOT NULL DEFAULT 'simple',
+	`visible` TINYINT(1) NOT NULL DEFAULT '0',
+	`release_id` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `name` (`name`),
+	INDEX `type` (`type`),
+	INDEX `visible` (`visible`),
+	INDEX `release_id` (`release_id`),
+	CONSTRAINT `FK_toggle_release` FOREIGN KEY (`release_id`) REFERENCES `release` (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `group_policy` (
+  `group_id` VARCHAR(128) NOT NULL,
+	`toggle_id` INT(11) NOT NULL,
+	`active` TINYINT(1) NOT NULL,
+	PRIMARY KEY (`group_id`, `toggle_id`),
+	INDEX `active` (`active`),
+	INDEX `FK_group_policy_toggle` (`toggle_id`),
+	CONSTRAINT `FK_group_policy_toggle` FOREIGN KEY (`toggle_id`) REFERENCES `toggle` (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `user_policy` (
-  `user_id` varchar(128) NOT NULL,
-  `toggle_id` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`user_id`,`toggle_id`),
-  KEY `active` (`active`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `user_id` VARCHAR(128) NOT NULL,
+	`toggle_id` INT(11) NOT NULL,
+	`active` TINYINT(1) NOT NULL DEFAULT '1',
+	PRIMARY KEY (`user_id`, `toggle_id`),
+	INDEX `active` (`active`),
+	INDEX `FK_user_policy_toggle` (`toggle_id`),
+	CONSTRAINT `FK_user_policy_toggle` FOREIGN KEY (`toggle_id`) REFERENCES `toggle` (`id`)
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB;
--- sql/2015-09-08-13-02-marketing-info-schema.sql
+++ sql/2015-09-08-13-02-marketing-info-schema.sql
@@ -1,14 +1,3 @@
-CREATE TABLE IF NOT EXISTS `release` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`info` VARCHAR(255) NOT NULL,
-	`visibility` TINYINT(1) NOT NULL DEFAULT 1,
-	`release_date` DATE NULL DEFAULT NULL,
-	PRIMARY KEY (`id`)
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB;
-
 CREATE TABLE IF NOT EXISTS `toggle_marketing_information` (
 	`toggle_id` INT(11) NOT NULL,
 	`screenshot_urls` VARCHAR(128) NULL DEFAULT NULL,
