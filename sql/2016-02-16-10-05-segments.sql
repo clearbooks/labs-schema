932b55cebd9bf6f2e67b1259f12ded45499af5d9
Horváth Péter <peter@clearbooks.co.uk>
2016-02-16 10:43:30 +0000

    segment policy table

---
+++ sql/2016-02-16-10-05-segments.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `segment_policy` (
+  `segment_id` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `toggle_id` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `segment_policy`
+  ADD PRIMARY KEY (`segment_id`,`toggle_id`),
+  ADD KEY `FK_segment_policy_toggle` (`toggle_id`);
+
+ALTER TABLE `segment_policy`
+  ADD CONSTRAINT `FK_segment_policy_toggle` FOREIGN KEY (`toggle_id`) REFERENCES `toggle` (`id`);
